7a6a7bb94506fdbab459dcc7d3baaaaaa0214a40
BassemBG <bgbassem8@gmail.com>
2023-06-14 20:25:52 +0100

    added new posts component, added store/devtools to debug with it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter/counter.component';
+import { PostsListComponent } from './posts/posts-list/posts-list.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'counter', component: CounterComponent },
+  { path: 'posts', component: PostsListComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -12,6 +12,8 @@ import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { PostsListComponent } from './posts/posts-list/posts-list.component';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 
 
@@ -23,14 +25,21 @@ import { HeaderComponent } from './shared/components/header/header.component';
     CounterButtonsComponent,
     CustomCounterInputComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    PostsListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    StoreModule.forRoot({counter: counterReducer}) //counter is the name that will be used inside components and the assigned property is the reducer . we can add more state: reducer -> needs to be the same states as in store 
-  
+    StoreModule.forRoot({counter: counterReducer}), //counter is the name that will be used inside components and the assigned property is the reducer . we can add more state: reducer -> needs to be the same states as in store 
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: !isDevMode(), // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
